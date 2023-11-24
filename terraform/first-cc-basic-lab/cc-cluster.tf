862fee68ecf0d36065c38e82c775960144881c59
eljeko <stefano.linguerri@gmail.com>
2023-11-24 16:56:43 +0100

    update

--- terraform/first-cc-basic-lab/cc-cluster.tf
+++ terraform/first-cc-basic-lab/cc-cluster.tf
@@ -2,15 +2,32 @@
 # Cluster
 # --------------------------------------------------------
 resource "confluent_kafka_cluster" "simple_cluster" {
-    display_name = "${local.cluster_name}"
-    availability = "SINGLE_ZONE"
-    cloud = "AWS"
-    region = "eu-south-1"
-    basic {}
-    environment {
-        id = confluent_environment.simple_env.id
-    }
-    lifecycle {
-        prevent_destroy = false
-    }
+  display_name = local.cluster_name
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "eu-south-1"
+  basic {}
+  environment {
+    id = confluent_environment.simple_env.id
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "confluent_kafka_topic" "orders" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.simple_cluster.id
+  }
+  topic_name    = "orders"
+  rest_endpoint = confluent_kafka_cluster.simple_cluster.rest_endpoint
+  
+  credentials {
+    key    = confluent_api_key.app_manager_kafka_cluster_key.id
+    secret = confluent_api_key.app_manager_kafka_cluster_key.secret
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
--- terraform/first-cc-basic-lab/output.tf
+++ terraform/first-cc-basic-lab/output.tf
@@ -1,14 +1,48 @@
 output "build" {
-  value = {
-    app_manager_kafka_cluster_key_KEY       = confluent_api_key.app_manager_kafka_cluster_key.id,
-    app_manager_kafka_cluster_key_SECRET    = nonsensitive(confluent_api_key.app_manager_kafka_cluster_key.secret),
+  /*value = {
+    R_01_app_manager_kafka_cluster_key_KEY       = confluent_api_key.app_manager_kafka_cluster_key.id,
+    R_01_app_manager_kafka_cluster_key_SECRET    = nonsensitive(confluent_api_key.app_manager_kafka_cluster_key.secret),
 
-    sr_cluster_key_KEY                      = confluent_api_key.sr_cluster_key.id,
-    sr_cluster_key_SECRET                   = nonsensitive(confluent_api_key.sr_cluster_key.secret),
+    R_02_sr_cluster_key_KEY                      = confluent_api_key.sr_cluster_key.id,
+    R_02_sr_cluster_key_SECRET                   = nonsensitive(confluent_api_key.sr_cluster_key.secret),
     
-    clients_kafka_cluster_key_KEY           = confluent_api_key.clients_kafka_cluster_key.id,
-    clients_kafka_cluster_key_SECRET        = nonsensitive(confluent_api_key.clients_kafka_cluster_key.secret),
+    R_03_clients_kafka_cluster_key_KEY           = confluent_api_key.clients_kafka_cluster_key.id,
+    R_03_clients_kafka_cluster_key_SECRET        = nonsensitive(confluent_api_key.clients_kafka_cluster_key.secret),
     
-  }
-   
+    R_04_____cluster_url                         = confluent_kafka_cluster.simple_cluster.bootstrap_endpoint
+  }*/
+
+  value = <<-EOT
+
+  ########################################
+  #       Cluster Setup Info             #
+  ########################################
+
+  Environment ID: ${confluent_environment.simple_env.id}
+  
+  Kafka Cluster
+  ID: ${confluent_kafka_cluster.simple_cluster.id}
+  Bootstrap URL: ${confluent_kafka_cluster.simple_cluster.bootstrap_endpoint}
+     Topic name: [${confluent_kafka_topic.orders.topic_name}]
+
+  Service Accounts and their Kafka API Keys (API Keys inherit the permissions granted to the owner):
+
+  ${confluent_service_account.app_manager.display_name}
+       SA ID: ${confluent_service_account.app_manager.id}
+     API Key: "${confluent_api_key.app_manager_kafka_cluster_key.id}"
+  API Secret: "${nonsensitive(confluent_api_key.app_manager_kafka_cluster_key.secret)}"
+
+  ${confluent_service_account.sr.display_name} 
+       SA ID: ${confluent_service_account.sr.id}
+     API Key: "${confluent_api_key.sr_cluster_key.id}"
+  API Secret: "${nonsensitive(confluent_api_key.sr_cluster_key.secret)}"
+
+  ${confluent_service_account.clients.display_name}
+       SA ID: ${confluent_service_account.clients.id}
+     API Key: "${confluent_api_key.clients_kafka_cluster_key.id}"
+  API Secret: "${nonsensitive(confluent_api_key.clients_kafka_cluster_key.secret)}"
+
+
+  EOT
+
 }
